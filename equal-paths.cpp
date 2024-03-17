fa24aa26e662b89e459e68678b136a82108544de
Jasmine Gau <gauj@usc.edu>
2024-03-17 07:14:43 +0000

    equal-paths.cpp

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -1,6 +1,8 @@
 #ifndef RECCHECK
 //if you want to add any #includes like <iostream> you must do them here (before the next endif)
-
+#include "equal-paths.h"
+#include <utility> 
+#include <algorithm> 
 #endif
 
 #include "equal-paths.h"
@@ -8,11 +10,23 @@ using namespace std;
 
 
 // You may add any prototypes of helper functions here
+pair<int, bool> checkPaths(Node* node) {
+  if(node == nullptr){
+    return (make_pair(0, true));
+  }
+  pair<int, bool> left = checkPaths(node->left);
+  pair<int, bool> right = checkPaths(node->right);
 
+  if(!left.second || !right.second || left.first!=right.first){
+    return (make_pair(0, false));
+  }
+
+  return std::make_pair(std::max(left.first, right.first) + 1, true);
+}
 
 bool equalPaths(Node * root)
 {
     // Add your code below
-
+  return checkPaths(root).second;
 }
 
